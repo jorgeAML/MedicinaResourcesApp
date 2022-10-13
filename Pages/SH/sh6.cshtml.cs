6b8e11152137ae4a1b803fdea2e1b54b23fb6b48
JorgeAML <martjorge8@gmail.com>
2022-10-13 12:33:47 -0600

    Test of the day

--- Pages/SH/sh6.cshtml.cs
+++ Pages/SH/sh6.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.Design;
+using System.Security.Policy;
 
 namespace MedicinaResourcesApp.Pages;
 
 public class IndexModelSH6 : PageModel
 {
     private readonly ILogger<IndexModelSH6> _logger;
+    public string MiMensaje { get; private set; } = "PageModel in C#";
+    public string Parrafo1 = "HOla hola ";
 
     public IndexModelSH6(ILogger<IndexModelSH6> logger)
     {
@@ -14,6 +18,7 @@ public class IndexModelSH6 : PageModel
 
     public void OnGet()
     {
-
+        MiMensaje = "Hola este texto cambiarlo 4565";
+        Parrafo1;
     }
 }
