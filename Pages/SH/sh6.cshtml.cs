aca2bf1cf4f2ea66fc3ac8ffbab9d9335e4281e5
JorgeAML <martjorge8@gmail.com>
2022-10-13 14:18:09 -0600

    Pruebas en C#

--- Pages/SH/sh6.cshtml.cs
+++ Pages/SH/sh6.cshtml.cs
@@ -9,7 +9,7 @@ public class IndexModelSH6 : PageModel
 {
     private readonly ILogger<IndexModelSH6> _logger;
     public string MiMensaje { get; private set; } = "PageModel in C#";
-    
+    public string Parrafo { get; private set; } = "PageModel in C#";
 
     public IndexModelSH6(ILogger<IndexModelSH6> logger)
     {
@@ -18,6 +18,9 @@ public class IndexModelSH6 : PageModel
 
     public void OnGet()
     {
-        
+        MiMensaje = "Antropologías, senato, música, JorgeAML Ú";
+        Parrafo = "En mi corta estadía en el instituto de Investigaciones históricas, Antropológicas y Arqueológicas a finales del año 1987 se implementó el inicio de un programa que se tituló:";
+
+
     }
 }
