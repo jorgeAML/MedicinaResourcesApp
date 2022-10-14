0b7ca7245e3cb1109117137980c9ee04395ceba2
JorgeAML <martjorge8@gmail.com>
2022-10-13 20:10:05 -0600

    update

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+console.log("Hello");
+let p2h = document.querySelector(".P2H");
+p2h.innerHTML = "Aquellos sectores o áreas no vinculadas con las estratégicas, se convierten en zonas de reserva que, en algunos casos, incuban las condiciones para el aparecimiento del cultivo o producto que para a ser estratégico dentro de la economía de exportación.";
