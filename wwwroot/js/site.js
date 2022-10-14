065da53f777fd594eb42b6c3b5484bd465d9f183
JorgeAML <martjorge8@gmail.com>
2022-10-13 20:16:42 -0600

    pagina 4

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,4 +4,6 @@
 // Write your JavaScript code.
 console.log("Hello");
 let p2h = document.querySelector(".P2H");
+let p2i = document.querySelector(".P2I");
 p2h.innerHTML = "Aquellos sectores o áreas no vinculadas con las estratégicas, se convierten en zonas de reserva que, en algunos casos, incuban las condiciones para el aparecimiento del cultivo o producto que para a ser estratégico dentro de la economía de exportación.";
+p2i.innerHTML = "La existencia de zonas estratégicas, zonas marginales y zonas de reserva o no integradas genera un paisaje económico, social y político, disperso y fuertemente atomizado que solo se unifica por la necesidad de los grupos hegemónicos de contar más allá de las zonas estratégicas, con una permanente y garantizada producción de artículos para el consumo fundamental de la sociedad, pero básicamente de ellos mismos. Y una zona de reserva de tierras y de fuerza de trabajo. "
